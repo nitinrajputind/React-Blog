982799922aca828cbdd88857546a83d98fd1fedf
nitinrajputind <nitinrajput971625@gmail.com>
2023-08-07 15:37:12 +0530

    Project is Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Components/Footer';
 import Header from './Components/Header';
 import RouterComp from './Routes/RouterComp';
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Header/>
     <RouterComp/>
+    <Footer/>
     </>
   );
 }
